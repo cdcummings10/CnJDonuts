941631e6b0030f7d460a16693111ddae1bef3b4c
cdcummings10 <ccummings0913@gmail.com>
2019-11-20 11:19:50 -0800

    Added registration functionality. Added user DB and saving funtionality.

--- DonutShop/DonutShop/Startup.cs
+++ DonutShop/DonutShop/Startup.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DonutShop.Data;
+using DonutShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +17,32 @@ namespace DonutShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public IHostEnvironment Environment { get; }
+        public Startup(IHostEnvironment environment)
+        {
+            Environment = environment;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string userConnectionString = Environment.IsDevelopment()
+                ? Configuration["ConnectionStrings:UserDefaultConnection"]
+                : Configuration["ConnectionStrings:DefaultConnection"];
+
+            services.AddDbContext<UserDbContext>(options =>
+            options.UseSqlServer(userConnectionString));
+
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<UserDbContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,11 +54,12 @@ namespace DonutShop
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
