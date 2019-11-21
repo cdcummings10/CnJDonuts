bab26dab571db223411913575f89b22a87ef8978
cdcummings10 <ccummings0913@gmail.com>
2019-11-21 08:15:43 -0800

    Added Logout Functionality

--- DonutShop/DonutShop/Pages/Account/Logout.cshtml.cs
+++ DonutShop/DonutShop/Pages/Account/Logout.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DonutShop.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,23 @@ namespace DonutShop.Pages.Account
 {
     public class LogoutModel : PageModel
     {
+        private SignInManager<ApplicationUser> _signInManager;
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public void OnGet()
         {
 
+        }
+        /// <summary>
+        /// Signs out the user using SignInManager.
+        /// </summary>
+        /// <returns>Redirects user to Home Page.</returns>
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
